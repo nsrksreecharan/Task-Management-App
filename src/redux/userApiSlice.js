e2099334a00acbdf036590e480ece12fef42b60a
N S R K Sree Charan <srkcharan@kitaab.biz>
2025-08-11 23:36:25 +0530

    changed base url

--- src/redux/userApiSlice.js
+++ src/redux/userApiSlice.js
@@ -4,17 +4,11 @@ import { toast } from "react-toastify";
 export const userApiSlice=createApi({
     reducerPath:"authApi",
     baseQuery:fetchBaseQuery({
-        baseUrl:"https://task-management-with-auth-bk5rjlm17-nsrksreecharans-projects.vercel.app/",
+        baseUrl:"https://task-management-with-auth.vercel.app/",
         prepareHeaders:(headers)=>{
             
             headers.set("Content-Type", "application/json");
 
-            const user=JSON.parse(localStorage.getItem("user")|| "{}");
-            
-            const token = user?.data?.token;
-            if(token){
-                headers.set("authorization",`Bearer ${token}`);
-            }
             return headers;
         },
     }),
